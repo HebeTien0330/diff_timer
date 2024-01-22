54087cdf5ea16c7eab0493b2126102dba2d341db
tangchengqin <527094604@qq.com>
2024-01-22 20:50:19 +0800

    #new version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/HebeTien0330/diff_timer'
 EMAIL = '527094604@qq.com'
 AUTHOR = 'Clarify'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
